bfd36632973238018040035c21d5ceabde68ac42
20120593 <20120593@student.hcmus.edu.vn>
2022-12-10 08:40:39 +0700

    GROUP09-50 init database

--- app/src/main/java/com/example/recorder/ListRecord.java
+++ app/src/main/java/com/example/recorder/ListRecord.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,11 +30,44 @@ public class ListRecord extends Activity {
     List<String> items_date = new ArrayList<String>();
 //    name
 
+    //database
+    InitDatabase database;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "OnCreated", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "OnCreated", Toast.LENGTH_SHORT).show();
+
+        //database
+
+        database = new InitDatabase(this, "data.sqlite", null, 1);
+        database.QueryData("DROP TABLE IF EXISTS Records");
+        database.QueryData("CREATE TABLE IF NOT EXISTS Records(id INTEGER, name VARCHAR(255), dateSave TEXT, length TEXT)");
+        Toast.makeText(this, "create table", Toast.LENGTH_SHORT).show();
+        database.QueryData("INSERT INTO Records(id, name, dateSave, length) VALUES(null, 'datarecord01', datetime('now'), time(datetime('now')))");
+        database.QueryData("INSERT INTO Records(id, name, dateSave, length) VALUES(null, 'datarecord02', datetime('now'), time(datetime('now')))");
+        database.QueryData("INSERT INTO Records(id, name, dateSave, length) VALUES(null, 'datarecord03', datetime('now'), time(datetime('now')))");
+        Toast.makeText(this, "insert table", Toast.LENGTH_SHORT).show();
+
+        Cursor data = database.GetData("SELECT * FROM Records");
+        while(data.moveToNext()) {
+            Integer id = data.getInt(0);
+            String name = data.getString(1);
+            String date = data.getString(2);
+            String time = data.getString(3);
+            Toast.makeText(this, id.toString(), Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, date, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, time, Toast.LENGTH_SHORT).show();
+            items_title.add(name);
+            items_length.add(time);
+            items_date.add(date);
+            Toast.makeText(this, "insert data to list", Toast.LENGTH_SHORT).show();
+
+
+        }
 
         listView = (ListView) findViewById(R.id.list_rc);
 //        progressBar = (ProgressBar) findViewById(R.id.list_item_progress);
